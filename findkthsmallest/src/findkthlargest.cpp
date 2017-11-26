c6a82e975e64f544d8baf2e2d78f86f46b9f7f2c
Antonio Roldán <Antonio7.9@hotmail.com>
2017-11-26 12:35:50 +0100

    Comments added

--- findkthsmallest/src/findkthlargest.cpp
+++ findkthsmallest/src/findkthlargest.cpp
@@ -9,37 +9,38 @@ void swap(int i, int j) {
 
 int partition(int array[], int left, int right, int pivot){
     int i = left, j = right;
-    while (i <= j) {
-        while (array[i] < pivot)
+    while (i <= j) { //We are iterating back and forth simultaneously throughout the array 
+        while (array[i] < pivot) //For as long as our forward traversal is less than the pivot 
             i++;
-        while (array[j] > pivot)
+        while (array[j] > pivot) //For as long as our backwards traversal is greater than the pivot 
             j--;
-        if (i <= j) {
+        if (i <= j) { //Variables are swapped to satisfy the fundamental condition of the quicksort algorithm 
             swap(array[i], array[j]);
             i++;
             j--;
         }
     };
+    //We made sure that all elements smaller than the pivot are found on the left and those greater than the pivot are on the right 
     return i;
 }
 
 int quickselect(int array[], int left, int right, int k){
-    k -= 1;
+    k -= 1; //Value readjustment to ensure that we will get the right output 
     while(1){
-        srand(time(NULL));
-        if (left == right){ //We return the only element there is if the array contains only one element
+        srand(time(NULL)); //C++ idiom to generate random elements 
+        if (left == right){ 
             return array[left];
-        }
+        } 
         int pivotIndex = left + rand() % (right - left + 1);
         int pivot = array[pivotIndex];
         pivotIndex = partition(array, left, right, pivot);
-        if (k == pivotIndex){
-            return array[k];
-        } else if (k < pivotIndex){
+        if (k == pivotIndex){ //In our base case we have already found the kth smallest element thanks to the condition 
+            return array[k]; //that there are k - 1 elements before the pivot that are smaller 
+        } else if (k < pivotIndex){ //We constantly swing between right and left as we assign random pivots on the left  
             right = pivotIndex - 1;
-        } else{
+        } else{ //or the right side of our array 
             left = pivotIndex + 1;
-        }
+        } //Until our pivot is equal to the number we are looking for 
     }
 }
 
